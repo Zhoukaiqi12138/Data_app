7188314fbd2b08a454921ac076594b9c6529b738
Zhoukaiqi12138 <148868661+Zhoukaiqi12138@users.noreply.github.com>
2024-09-23 11:02:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-st.title('California Housing Data (1990) )
+st.title('California Housing Data (1990) ‘)
 df = pd.read_csv('housing.csv')
 
 price_filter = st.slider('Minimal Median House Price', 0, 500001, 200000)
